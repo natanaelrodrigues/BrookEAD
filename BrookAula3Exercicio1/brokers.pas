1f3e64e12ad898b884267109c92c68fd5d214d17
silvioprog <silvioprog@gmail.com>
2014-08-03 23:36:19 -0300

    Formatando as mensagens de erro conforme os modos de build escolhidos.

--- BrookAula3Exercicio1/brokers.pas
+++ BrookAula3Exercicio1/brokers.pas
@@ -5,16 +5,29 @@ unit Brokers;
 interface
 
 uses
-  {$IfDef RELEASE}
-    BrookFCLCGIBroker
-  {$Else}
-    BrookFCLHttpAppBroker
-  {$EndIf}
-  ,BrookUtils
-  ;
+{$IFDEF DEBUG}
+  BrookFCLHttpAppBroker
+{$ELSE}
+  BrookFCLCGIBroker
+{$ENDIF},
+  BrookUtils, BrookHttpConsts;
 
 implementation
+
 initialization
-  BrookSettings.Port := 8183;
+{$IFDEF DEBUG}
+  BrookSettings.Port := 8080;
+{$ENDIF}
+  BrookSettings.Page404 :=
+    '<html><head><title>Página não encontrada</title></head><body>' +
+    '<h1>404 - Página não encontrada.</h1></body></html>';
+  BrookSettings.Page500 :=
+    '<html><head><title>Erro interno do servidor</title></head><body>' +
+    '<h1>500 - Erro interno do servidor.</h1>' +
+    '<p style="color: red;">@error</p>' +
+{$IFDEF TRACE}
+    '<pre style="color: gray;">@trace</pre>' +
+{$ENDIF}
+    '</body></html>';
 
 end.
